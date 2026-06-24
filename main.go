8dda4cda7290a41e3bf06adf45061dc33171419b
D3ssnk <65511755+D3ssnk@users.noreply.github.com>
2026-06-24 16:33:17 +0100

    implemented endpoint that takes the url path and returns the data

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"flag"
+	"fmt"
+	"io"
+
 	//"net/http"
 	"net/http"
 	"net/url"
@@ -10,23 +12,39 @@ import (
 )
 
 func main() {
-    portPtr := flag.Int("port", 3000, "The port on which the caching proxy server will run.")
-	originPtr := flag.String("origin","", "The URL of the server to which the requests will be forwarded.")
+	portPtr := flag.Int("port", 3000, "The port on which the caching proxy server will run.")
+	originPtr := flag.String("origin", "", "The URL of the server to which the requests will be forwarded.")
 	flag.Parse()
 
 	origin := *originPtr
-	port := fmt.Sprint(*portPtr)
-	if  parsedURL, err := url.Parse(origin); err != nil || parsedURL.Host == "" ||
-	(!strings.HasPrefix(origin,"http://") && !strings.HasPrefix(origin,"https://"))  {
+	port := ":" + fmt.Sprint(*portPtr)
+
+	if parsedURL, err := url.Parse(origin); err != nil || parsedURL.Host == "" ||
+		(!strings.HasPrefix(origin, "http://") && !strings.HasPrefix(origin, "https://")) {
 		panic("Bad!")
 	}
-	
-	// http.HandleFunc("/", handler)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fullURL := origin + r.URL.Path
+		resp, err := http.Get(fullURL)
+		if err != nil {
+			w.WriteHeader(resp.StatusCode)
+			fmt.Fprintf(w, "%s", err.Error())
+			return
+		}
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			w.WriteHeader(resp.StatusCode)
+			fmt.Fprintf(w, "%s", err.Error())
+			return
+		}
+
+		w.Header().Add("X-Cache", "MISS")
+		w.WriteHeader(resp.StatusCode)
+		w.Write(body)
+	})
 
 	http.ListenAndServe(port, nil)
 }
-
-// func handler (w http.ResponseWriter, r *http.Request) {
-// 	origin 
-// 	resp, err := http.Get()
-// }
