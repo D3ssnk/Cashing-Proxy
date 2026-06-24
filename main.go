5daeef1c624da7a9dc537649804886419e1abc81
D3ssnk <65511755+D3ssnk@users.noreply.github.com>
2026-06-24 16:05:28 +0100

    added validation to the origin flag and set up a serve on the port flag

--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"flag"
+	//"net/http"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 func main() {
     portPtr := flag.Int("port", 3000, "The port on which the caching proxy server will run.")
 	originPtr := flag.String("origin","", "The URL of the server to which the requests will be forwarded.")
-
 	flag.Parse()
-	fmt.Println(*portPtr, *originPtr)
+
+	origin := *originPtr
+	port := fmt.Sprint(*portPtr)
+	if  parsedURL, err := url.Parse(origin); err != nil || parsedURL.Host == "" ||
+	(!strings.HasPrefix(origin,"http://") && !strings.HasPrefix(origin,"https://"))  {
+		panic("Bad!")
+	}
+	
+	// http.HandleFunc("/", handler)
+
+	http.ListenAndServe(port, nil)
 }
+
+// func handler (w http.ResponseWriter, r *http.Request) {
+// 	origin 
+// 	resp, err := http.Get()
+// }
